4c149cb1ff360cf60cf0091bd9e7a09549f15a41
PiotrAndrzejewsky <piotrandrzejewski@onet.eu>
2022-10-11 12:16:08 +0200

    feat: add order_types and orders schema

--- schemas/db-schema.sql
+++ schemas/db-schema.sql
@@ -3,7 +3,27 @@ CREATE DATABASE ordersApp;
 USE ordersApp;
 
 CREATE TABLE users (
-user_id INT AUTO_INCREMENT PRIMARY KEY,
+id INT AUTO_INCREMENT PRIMARY KEY,
 username VARCHAR(50) NOT NULL,
 password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE order_types (
+id INT AUTO_INCREMENT PRIMARY KEY,
+user_id INT NOT NULL,
+name VARCHAR(100),
+FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE orders (
+id INT AUTO_INCREMENT PRIMARY KEY,
+user_id INT NOT NULL,
+order_type_id INT NOT NULL DEFAULT 0,
+price INT NOT NULL,
+title VARCHAR(50) NOT NULL,
+quantity INT NOT NULL,
+plannedCompletionDate DATE NOT NULL,
+completed BIT NOT NULL DEFAULT 0,
+FOREIGN KEY (user_id) REFERENCES users(id),
+FOREIGN KEY (order_type_id) REFERENCES order_types(id)
 );
